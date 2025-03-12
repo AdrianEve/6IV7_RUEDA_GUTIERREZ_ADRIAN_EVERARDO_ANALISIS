c37d9839a8c2a5ffe70c1201c5a4e074e39de383
AdrianEve <rgaeverardo@gmail.com>
2025-03-11 23:19:09 -0600

    Delete 03Fichero.py

--- 03Fichero.py
+++
@@ -1,8 +0,0 @@
-#vAMOS A HACER UN EJEMPLO DE CARFA DE ARCGIVO Y APKICAR MIN, MAX, MEDIA, DESCIACION ENTANDAR
-
-import pandas as pd
-def cotizacion(fichero):
-    df=pd.read_csv(fichero,sep=';', decimal=',', thousands='.', index_col=0)
-    return pd.DataFrame([df.min(), df.max(), df.mean(), df.std()], index=['Minimo', 'Maximo', 'Media', 'desviacion estandar'])
-
-print(cotizacion('./EstadisticaDescriptiva/cotizacion.csv'))
